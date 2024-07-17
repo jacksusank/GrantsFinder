d26ed118ba0933e933836d3d1046dc8832278dc6
JACK_SUSANK <jsusank5@gmail.com>
2024-07-17 16:09:09 -0700

    test without ragrants_db

--- verify_db.py
+++ verify_db.py
@@ -1,16 +1,17 @@
 from app import app, db
 from models import Opportunity
+from utils import delete_opportunity
 
 with app.app_context():
-    # Create a new opportunity instance
-    opportunity = Opportunity(opportunity_id=123456, long_text='Sample text', embedding_vector=[0.1, 0.2, 0.3])
-
-    # Add the opportunity to the session
-    db.session.add(opportunity)
-
-    # Commit the session
+    # Delete the last 200 opportunities in my database
+    opportunities = Opportunity.query.all()
+    for opportunity in opportunities[-200:]:
+        opportunity_id = opportunity.opportunity_id
+        delete_opportunity(opportunity_id)
     db.session.commit()
 
-    # Query the database to verify
-    opportunities = Opportunity.query.all()
-    print(opportunities)
+
+
+
+
+
